28fa1f764c44a1dd612cfb09577574602833666e
Kevin <kevhines@gmail.com>
2021-05-12 16:51:48 -0400

    commit tree 91efb27f1cff4e3c94a9cd3a5df80cd106d752fe

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+        focusHandler = () => {console.log('Good!')}
+        blurHandler = () => {console.log('Hey! Eyes on me!')}
+
+    render() {
+        return (
+       <div><button onFocus={this.focusHandler} onBlur={this.blurHandler} type="button">I am a button</button></div>
+        )
+      }
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+    
+    handleKeyUp = (e) => {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return (
+        <div>
+           <input id="keypad" type="password" onKeyUp={this.handleKeyUp}></input>
+        </div>
+        )
+      }
+
+}
